3f9be4dda2d6d3221844b3254a8bc986db51e23d
AnaMitova <mitova.a24097@next.edu.mk>
2026-07-05 00:29:51 +0200

    Logo change

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,11 +2,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
 
     <link href="./output.css" rel="stylesheet" type="text/css"/>
     
-    
+    <link rel="icon" type="image/png" href="./img/image.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
